0bcae398131069993e5fe02b354e63ac0817c0ec
qouillyan <stojakovicdragan98@gmail.com>
2022-11-24 18:31:07 +0100

    Dodao sam tabelu movies

---
+++ database/migrations/2022_11_24_171729_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 128);
+            $table->string('genre', 64);
+            $table->string('director', 128);
+            $table->integer('year');
+            $table->text('plot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
+
+
